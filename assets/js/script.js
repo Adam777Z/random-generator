0332025cece325016be13e4e21ae26ef07f33024
Adam777 <Adam777-5867@outlook.com>
2021-10-29 17:00:33 +0000

    Progressive Web App (PWA) update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js');
+}
+
 var prefix = '', suffix = '', length = 8, random = '', result;
 
 document.addEventListener( 'DOMContentLoaded', (event) => {
---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'random-generator-cache';
+const cacheVersion = '1.0.0'; // Needed for service worker auto update
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.delete(cacheName).then((deleted) => {
+			caches.open(cacheName).then((cache) => cache.addAll([
+				'/random-generator/',
+				'/random-generator/manifest.json',
+				'/random-generator/assets/images/icon.png',
+				'/random-generator/assets/images/icon-192x192.png',
+				'/random-generator/assets/css/bootstrap.min.css',
+				'/random-generator/assets/css/bootstrap-icons.min.css',
+				'/random-generator/assets/css/fonts/bootstrap-icons.woff2',
+				// '/random-generator/assets/css/style.min.css',
+				'/random-generator/assets/js/bootstrap.bundle.min.js',
+				'/random-generator/assets/js/script.min.js',
+			]));
+		})
+	);
+});
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.open(cacheName).then((cache) => cache.match(event.request).then((response) => response || fetch(event.request))));
+});
